c012fce223ff95c1d96ea3cdc34ae66a0e3ead14
mingineer <wsley040@gmail.com>
2023-02-04 23:52:36 +0900

    주석

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# 어드민페이지 테스트
 def hello_world(request):
     return HttpResponse('Admin Page Account Test')
--- adminpage/settings.py
+++ adminpage/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import environ
 import os
 
+# env 설정 #############
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -23,6 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+#############################
+
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
