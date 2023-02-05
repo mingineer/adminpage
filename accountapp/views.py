e7dd2ca3e2aec6363ed2c3b4196e902f3f381646
mingineer <wsley040@gmail.com>
2023-02-05 21:18:08 +0900

    reverse POST call

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from accountapp.models import HelloWorld
 
@@ -7,7 +9,6 @@ from accountapp.models import HelloWorld
 
 # test admin page
 def hello_world(request):
-
     if request.method == "POST":
 
         temp = request.POST.get('hello_world_input')
@@ -16,6 +17,8 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
