92a5f8f9c162c1e778ec58bd1f24b7abf01c4a6b
mingineer <wsley040@gmail.com>
2023-02-06 22:46:14 +0900

    create login page

--- adminpage/settings.py
+++ adminpage/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import environ
 import os
 
+from django.urls import reverse_lazy
+
 # env Settings #############
 env = environ.Env(
     # set casting, default value
@@ -134,3 +136,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
